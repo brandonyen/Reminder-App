0b93de4da5924f711472b87fe43190f916ac4667
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2023-10-19 12:04:06 -0400

    initialized project

---
+++ Reminder-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder-App
+//
+//  Created by Brandon Yen on 10/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
